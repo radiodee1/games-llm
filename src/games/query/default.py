1fa107222c6da0cb59aa3fe4c7b27b72258e3dc5
David Liebman <radiodee1@gmail.com>
2026-02-15 10:10:09 -0500

    default start

---
+++ src/games/query/default.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import os 
+import time
+import base64
+import argparse 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Default:
+
+    def __init__(self, model, streaming=False, chat=True, visual=True) -> None:
+        self.images = []
+        self.context = []
+        self.text = ''
+        self.result = ''
+        self.last_code = 0
+        self.model = model 
+        self.headers = {}
+        self.data = {} ## put payload here.
+        self.history = []
+
+        self.visual = visual ## visual or not visual
+        self.streaming = streaming ## streaming or not streaming
+        self.chat = chat ## chat or generate
+        self.think = True ## think or not think
+        self.url = 'http://localhost:11434/api/'
+
+        pass
+
+    def set_url(self, url):
+        self.url = url
+
+    def get_url(self):
+        return self.url
+
+    def image_to_string(self, image_path):
+        with open(image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+        return encoded_string
+        
+
+    def payload(self, image=None, context=None, text=None):
+        if image is not None and isinstance(image, str):
+            self.images.append(image)
+        if context is not None and isinstance(context, list):
+            self.context = context
+        if text is not None and isinstance(text, str):
+            self.text = text
+
+        if self.chat:
+            self.history += [{
+                "role": "system",
+                "content": self.text,
+                "images" : self.images
+            }]
+
+            self.data  = {
+                "model": self.model,
+                "messages": self.history,
+                "stream": self.streaming
+            }
+        else:
+            self.data = {
+                "model": self.model,
+                "prompt": self.text,
+                "images": self.images,
+                "context": self.context,
+                "stream": self.streaming
+            }
+
