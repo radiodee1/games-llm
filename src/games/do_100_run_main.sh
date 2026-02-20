f6db9541d896fb075a51911739290afa97074fdd
David Liebman <radiodee1@gmail.com>
2026-02-20 10:30:20 -0500

    threshold 100

--- src/games/do_100_run_main.sh
+++ src/games/do_100_run_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-uv run ./main.py --model gemini-3-flash-preview --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 80  --stream # --small_test 3 # --no_llm 1 
+uv run ./main.py --model gemini-3-flash-preview --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 100  --stream # --small_test 3 # --no_llm 1 
