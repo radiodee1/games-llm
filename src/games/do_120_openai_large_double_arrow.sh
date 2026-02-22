4a1e78ee807a67f0aeeb2e66fbf8e2d66f9edf1c
David Liebman <radiodee1@gmail.com>
2026-02-22 13:10:51 -0500

    do 120

--- src/games/do_120_openai_large_double_arrow.sh
+++ src/games/do_120_openai_large_double_arrow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-uv run ./main.py --model gpt-3.5 --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 100  --stream --double_arrow --inverse_size 1  # --small_test 3 # --no_llm 1 
+uv run ./main.py --model gpt-5.2 --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 100  --stream --double_arrow --inverse_size 1  # --small_test 3 # --no_llm 1 
