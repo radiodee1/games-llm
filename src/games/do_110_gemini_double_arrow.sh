50a87963acc3f6268151d48282f263dcb9108a8b
David Liebman <radiodee1@gmail.com>
2026-03-02 08:59:26 -0500

    inverse size for gemini 3 pro preview

--- src/games/do_110_gemini_double_arrow.sh
+++ src/games/do_110_gemini_double_arrow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-uv run ./main.py --model gemini-3-pro-preview --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 100  --stream --double_arrow  # --small_test 3 # --no_llm 1 
+uv run ./main.py --model gemini-3-pro-preview --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --thinking --threshold 100  --stream --double_arrow --inverse_size 1 # --small_test 3 # --no_llm 1 
