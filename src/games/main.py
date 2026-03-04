cd9ed69dd3c828c29daa1b99ae006b3e3881b93c
David Liebman <radiodee1@gmail.com>
2026-03-04 08:57:49 -0500

    goal etc.

--- src/games/main.py
+++ src/games/main.py
@@ -262,7 +262,7 @@ def draw(canvas):
         message = "Score!!"
         ball_init(True)
         
-    if int(ball_pos[0]) > WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) > int(paddle2_pos[1]) - HALF_PAD_HEIGHT and int(ball_pos[1]) < int(paddle2_pos[1]) + HALF_PAD_HEIGHT:  
+    if int(ball_pos[0]) > WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1] + ball_vel[1] // 2) > int(paddle2_pos[1]) - HALF_PAD_HEIGHT and int(ball_pos[1] + ball_vel[1] // 2) < int(paddle2_pos[1]) + HALF_PAD_HEIGHT:  
         ball_vel[0] = -ball_vel[0]
         ball_vel[0] *= (1.0 + acceleration)
         ball_vel[1] *= (1.0 + acceleration)
