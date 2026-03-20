ae578b4c4fc3ea6504ada5160d7daccb5070c103
David Liebman <radiodee1@gmail.com>
2026-03-20 11:03:11 -0400

    rm top_p from launcher

--- src/games/ale_10_pong.sh
+++ src/games/ale_10_pong.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-uv run ./main.py --plugin pong --model gpt-5.2 --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --threshold 100  --stream  --inverse_size 1 --top_p 0.01  --temperature 0.01  # --small_test 3 # --no_llm 1 
+uv run ./main.py --plugin pong --model gpt-5.2 --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3  --threshold 100  --stream  --inverse_size 1   --temperature 0.01  # --small_test 3 # --no_llm 1 
