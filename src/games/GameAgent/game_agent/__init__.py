d6e281a4989bf7b2036059e5abefc35d3d42f9d1
David Liebman <radiodee1@gmail.com>
2026-05-11 14:31:48 -0400

    whitelist for agents

--- src/games/GameAgent/game_agent/__init__.py
+++ src/games/GameAgent/game_agent/__init__.py
@@ -1,15 +1,16 @@
 from .default import DefaultPlugin, DefaultBare 
 from .pygame_pong import PygamePongAgent
 from .agent import LunarLanderAgent, PongAgent, BreakoutAgent
-
+from .pygame_dots import PygameDotAgent
 
 pluginlist = {
     'lunarlander'       : 'LunarLanderAgent',
     'pong'              : 'PongAgent',
     'pygamepong'        : 'PygamePongAgent',
     'pygame'            : 'PygamePongAgent',
-    'breakout'          : 'BreakoutAgent'
+    'breakout'          : 'BreakoutAgent',
+    'dots'              : 'PygameDotAgent'
 }
 
-pygamelist = [ 'pygamepong', 'pygame' ]
+pygamelist = [ 'pygamepong', 'pygame', 'dots' ]
 
