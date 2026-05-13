cce3e601fa66b9926206a84aad6dace055649145
David Liebman <radiodee1@gmail.com>
2026-05-13 12:34:46 -0400

    dots

--- src/games/ale_80_gemma4_dots.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-uv run ./main.py --plugin dots --model gemma4:e4b --sudden_death 2 --context_size -1 --image_strip -1 --q_len 1 --stream --inverse_size 1 --temperature 0.95 --skip 1 --no_pic --total 50 #  --top_p 0.01 # --small_test 3 # --no_llm 1 --thinking
