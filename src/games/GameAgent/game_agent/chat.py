71079389e38961db6608269da9415c005a5c2466
David Liebman <radiodee1@gmail.com>
2026-05-29 15:31:36 -0400

    chat.py file

---
+++ src/games/GameAgent/game_agent/chat.py
@@ -0,0 +1,12 @@
+from .default import DefaultBare
+
+
+class ChatAgent ( DefaultBare ):
+
+    def __init__(self, mode="rgb_array", inverse_size=4, show_image=True) -> None:
+        super().__init__(mode, inverse_size, show_image)
+        self.agent = 'Chat'
+        self.prompt_string = ""
+        self.action_meaning = []
+
+
