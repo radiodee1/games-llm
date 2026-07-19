5ffd196ec973838a0a3d3ce9b1a723214f6d41f1
David Liebman <radiodee1@gmail.com>
2026-07-19 09:35:41 -0400

    vjepa script

---
+++ src/games/vjepa_10_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+uv run ./main.py --model vjepa --plugin pong --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3 --thinking --threshold 100 --stream --inverse_size 4 --video 5
