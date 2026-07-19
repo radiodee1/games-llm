0cfb4bff3824cc62519e58bc05263b80c0b4229b
David Liebman <radiodee1@gmail.com>
2026-07-19 13:51:10 -0400

    no json output

--- src/games/LLM/llm_do/local.py
+++ src/games/LLM/llm_do/local.py
@@ -33,9 +33,9 @@ class OllamaImages (Oai): ## Oai
 class Vjepa2Demo (DefaultLLM):
     def __init__(self, model, streaming=False, chat=True, visual=True, think=False, key=None) -> None:
         super().__init__(model, streaming, chat, visual, think, key)
+        self.write_to_text = False  ## skip json output
         pass 
 
     def do(self, image=None, context=None, text=None):
         from notebooks.vjepa2_demo_cpu import run_sample_inference 
-
         return run_sample_inference()
