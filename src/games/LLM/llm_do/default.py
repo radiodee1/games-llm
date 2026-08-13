024f87cd53af2236fb242e1417d3c4ef0d71b776
David Liebman <radiodee1@gmail.com>
2026-08-13 09:20:55 -0400

    use short path

--- src/games/LLM/llm_do/default.py
+++ src/games/LLM/llm_do/default.py
@@ -60,6 +60,7 @@ class DefaultLLM:
         self.reasoning_effort = None ## 'none', 'low', 'medium', 'high'
         self.word_frequency = {}
         self.foldername = 'train'
+        self.use_short_path = True
         self.print('Default', model)
         pass
 
@@ -434,7 +435,11 @@ class DefaultLLM:
         self.print(scraped_output, class_labels, 'num <===')
         n = str('00000000' + str(num_string))[-5:]
         filename_string = './pic/' + self.foldername + '/video_image_label.json'
-        pic_string = os.path.join( os.path.expanduser('~') , self.write_json_directory  ,'pic/' + self.foldername + '/output_' + n + '.mp4' )
+        if not self.use_short_path:
+            pic_string = os.path.join( os.path.expanduser('~') , self.write_json_directory  ,'pic/' + self.foldername + '/output_' + n + '.mp4' )
+        else:
+            pic_string = os.path.join( '~' , self.write_json_directory  ,'pic/' + self.foldername + '/output_' + n + '.mp4' )
+
         self.write_json[pic_string] = scraped_output 
         self.print('===',self.write_json,'===')
         with open(filename_string, 'w') as w:
