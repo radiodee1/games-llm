6e15d67ce23f92491184bce480d423be2b3af9d7
David Liebman <radiodee1@gmail.com>
2026-08-21 07:30:52 -0400

    copy_scripts.sh

--- src/games/scripts/02_copy_scripts.sh
+++ src/games/scripts/02_copy_scripts.sh
@@ -6,4 +6,6 @@ cp -r !(02_copy_scripts.sh|03_copy_games.sh) ../../../../vjepa2/.
 
 cp classes_pong.json ../../../../VJEPA2_FILES/demo/json/.
 
+cp ../notebooks/* ../../../../vjepa2/notebooks/.
+
 echo "cp to vjepa2 folder"
