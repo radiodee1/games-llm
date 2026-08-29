acdd7b622abed8d8bc1b04d223722f1f0c7c15b6
David Liebman <radiodee1@gmail.com>
2026-08-29 15:28:14 -0400

    setup folders

--- src/games/scripts/09_setup_folders.sh
+++ src/games/scripts/09_setup_folders.sh
@@ -10,3 +10,5 @@ wget https://dl.fbaipublicfiles.com/vjepa2/evals/ssv2-vitl-16x2x3.pt
 
 wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2_1_vitl_dist_vitG_384.pt
 wget https://dl.fbaipublicfiles.com/vjepa2/evals/ssv2-vitg-384-64x2x3.pt
+
+wget https://dl.fbaipublicfiles.com/vjepa2/vitg-384.pt
