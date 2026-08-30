0617762e9d86bc631fc30abf03dd560052716722
David Liebman <radiodee1@gmail.com>
2026-08-30 10:20:28 -0400

    add key to run

--- src/games/scripts/vjepa_10_run.sh
+++ src/games/scripts/vjepa_10_run.sh
@@ -6,4 +6,4 @@ rm 00*.png 00*.json figure_x*.png
 
 cd ..
 
-uv run ./main.py --model vjepa --plugin ssv --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3 --thinking --threshold 100 --stream --inverse_size 4 --video 4 --no_pic
+uv run ./main.py --model vjepa --plugin ssv --sudden_death 2 --context_size 0 --image_strip -1 --q_len 3 --thinking --threshold 100 --stream --inverse_size 4 --video 4 --no_pic --key 21vitl
