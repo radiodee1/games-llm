dc0a3f80f64e400bf95ff962809b18798cc2cd51
David Liebman <radiodee1@gmail.com>
2026-08-30 10:16:37 -0400

    train 1000

---
+++ src/games/scripts/07_train_1000_corpus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#source ~/workspace/miniconda3/etc/profile.d/conda.sh
+
+#conda activate vjepa2-310
+
+uv run python -m notebooks.vjepa2_demo_cpu --image_span 1000 --save_checkpoint --foldername train --key 21vitl --batch 2
--- src/games/scripts/07_train_400_corpus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#source ~/workspace/miniconda3/etc/profile.d/conda.sh
-
-#conda activate vjepa2-310
-
-uv run python -m notebooks.vjepa2_demo_cpu --image_span 400 --save_checkpoint --foldername train
