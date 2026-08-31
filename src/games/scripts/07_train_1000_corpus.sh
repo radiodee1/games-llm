5c183c59a51bb5285c1d4771367dd8c08d65c670
David Liebman <radiodee1@gmail.com>
2026-08-31 07:38:39 -0400

    vitg option

--- src/games/scripts/07_train_1000_corpus.sh
+++ src/games/scripts/07_train_1000_corpus.sh
@@ -4,4 +4,4 @@
 
 #conda activate vjepa2-310
 
-uv run python -m notebooks.vjepa2_demo_cpu --image_span 1000 --save_checkpoint --foldername train --key 21vitl --batch 2
+uv run python -m notebooks.vjepa2_demo_cpu --image_span 1000 --save_checkpoint --foldername train --key vitg --batch 1
