385afcfec739f3896ddaa9a5b0d59e705237b675
David Liebman <radiodee1@gmail.com>
2026-09-13 12:07:22 -0400

    wget

--- src/games/scripts/09_setup_folders.sh
+++ src/games/scripts/09_setup_folders.sh
@@ -8,9 +8,9 @@ cd ~/workspace/VJEPA2_FILES/demo
 wget https://dl.fbaipublicfiles.com/vjepa2/vitl.pt ## <--
 wget https://dl.fbaipublicfiles.com/vjepa2/evals/ssv2-vitl-16x2x3.pt
 
-wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2_1_vitl_dist_vitG_384.pt
-wget https://dl.fbaipublicfiles.com/vjepa2/evals/ssv2-vitg-384-64x2x3.pt
+#wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2_1_vitl_dist_vitG_384.pt
+#wget https://dl.fbaipublicfiles.com/vjepa2/evals/ssv2-vitg-384-64x2x3.pt
 
-wget https://dl.fbaipublicfiles.com/vjepa2/vitg-384.pt
+#wget https://dl.fbaipublicfiles.com/vjepa2/vitg-384.pt
 
-wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2_1_vitg_384.pt
+#wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2_1_vitg_384.pt
