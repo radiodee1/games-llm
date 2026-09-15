24900b9560e35e0fc09882edd7ea391dfcbe242e
David Liebman <radiodee1@gmail.com>
2026-09-15 11:16:57 -0400

    uv

--- src/games/scripts/04_requirements_games.sh
+++ src/games/scripts/04_requirements_games.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ~/workspace/games-llm/src/games/
-
-# uv export --format requirements.txt --no-hashes --output-file ~/workspace/vjepa2_scripts/requirements-games.txt
+# curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo "does nothing"
