4f3f052abed868ece67168d57311454539631634
David Liebman <radiodee1@gmail.com>
2026-09-17 10:40:11 -0400

    tar csv

--- src/games/scripts/18_tar_csv_files.sh
+++ src/games/scripts/18_tar_csv_files.sh
@@ -1,4 +1,4 @@
-cd pic
+cd ~/workspace/VJEPA2_FILES/demo/pic
 
 tar --exclude='*.gz' --exclude='*.txt' --exclude='*.gif' --exclude='*.json' --exclude='*.mp4' --exclude='*.png' -czf csv_archive.tar.gz *
 
